a902707aa5cce0c7aed7e39cc3190ab2366c3269
DangMinhChau <dangminhchau80@gmail.com>
2024-05-20 18:14:54 +0700

    Add docs

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-root_dir = os.path.dirname(os.path.abspath(__file__))
-database_str = f'{root_dir}/database/mydatabase.db'
-admin_password = 'admin'
--- init.py
+++
@@ -1,7 +0,0 @@
-from views.appUI import App
-from utils import db
-
-if __name__ == '__main__':
-    db.create_tables()
-    app = App()
-    app.mainloop()
